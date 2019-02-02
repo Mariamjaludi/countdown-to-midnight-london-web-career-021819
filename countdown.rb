75fde2b082f724eebe17d29e937ad31a0de31152
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-02 10:42:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,14 @@
 def countdown(number)
   while number != 0
     puts "#{number} SECOND(S)!"
-    sleep(2)
+  end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  while number != 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
   end
   puts "HAPPY NEW YEAR!"
 end
